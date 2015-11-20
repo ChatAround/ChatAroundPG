9c47ba58281149371ba0b445462d9cfa96dc311f
Alexis Gkiourkas <kretinosgr@gmail.com>
2015-11-20 17:15:42 +0200

    Connection between web app and server via socket (NOT tested)

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,6 @@
 var x = document.getElementById("location");
 
+
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition,showError);
@@ -19,7 +20,7 @@ function showPosition(position) {
 }
 
 function showError(error) {
-    switch(error.code) {
+    switch (error.code) {
         case error.PERMISSION_DENIED:
             x.innerHTML = "User denied the request for Geolocation.";
             break;
@@ -30,4 +31,57 @@ function showError(error) {
             x.innerHTML = "The request to get user location timed out.";
             break;
     }
+}
+
+//socket
+
+function setConnected(connected) {
+    document.getElementById('connect').disabled = connected;
+    document.getElementById('disconnect').disabled = !connected;
+}
+var socket = new SockJS('http://test.pusher.com/'); //http://chataround.ddns.net:8080/login
+var stompClient = Stomp.over(socket);
+
+function connect() {
+    stompClient.connect({}, function(frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/chat', function(LoginMsg){
+            showLoginMesssage(JSON.parse(LoginMsg.body).content);
+        });
+    });
+}
+
+function showLoginMesssage(message) {
+    var response = document.getElementById('response');
+    var p = document.createElement('p');
+    p.style.wordWrap = 'break-word';
+    p.appendChild(document.createTextNode(message));
+    response.appendChild(p);
+}
+function disconnect() {
+    if (stompClient != null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendLogin(){
+
+    var username = $('#username').val();
+    //var password = $('#password').val();
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    var id = "1L";
+
+    var userInfo = {
+        id: id,
+        username: username,
+        //password: password,
+        latitude: latitude,
+        longitude: longitude
+    };
+
+    socket.send(JSON.stringify(userInfo));
 }
