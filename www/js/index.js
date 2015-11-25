4a84316822ddfafbc2639f34643490c3c5fb477c
Alexis Gkiourkas <kretinosgr@gmail.com>
2015-11-26 00:32:13 +0200

    Connection between web app and server via method POST using jquery and ajax.

--- www/js/index.js
+++ www/js/index.js
@@ -1,87 +1,122 @@
-var x = document.getElementById("location");
+var NS = {
+    currentLatitude: null,
+    currentLongitude: null
+};
 
+var showLocation = function () {
+    $("#latitude").html(NS.currentLatitude);
+    $("#longitude").html(NS.currentLongitude);
+};
 
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition,showError);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+var Location = {
+    updateLocation: function () {
+        var updateCurrentLocation = function (position) {
+            NS.currentLatitude = position.coords.latitude;
+            NS.currentLongitude = position.coords.longitude;
+
+            showLocation();
+        };
+
+        var throwError = function (error) {
+            switch (error.code) {
+                case error.PERMISSION_DENIED:
+                    throw new Error("User denied the request for Geolocation.");
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    throw new Error("Location information is unavailable.");
+                    break;
+                case error.TIMEOUT:
+                    throw new Error("The request to get user location timed out.");
+                    break;
+            }
+        };
+
+        navigator.geolocation.getCurrentPosition(updateCurrentLocation, throwError);
     }
-}
+};
 
-function showPosition(position) {
+setInterval(function () {
+    Location.updateLocation();
+}, 500);
 
-    var lat,long;
+var sendLoginInfo = function (e) {
+    e.preventDefault();
 
-    lat = position.coords.latitude;
-    long = position.coords.longitude;
+    var id = "1";
+    var username = $("[name='username']").val();
+    //var password = $("[name='password']")).val();
+    var latitude = NS.currentLatitude;
+    var longitude = NS.currentLongitude;
 
-    x.innerHTML = "Latitude: " + lat + "<br>Longitude: " + long;
-}
-
-function showError(error) {
-    switch (error.code) {
-        case error.PERMISSION_DENIED:
-            x.innerHTML = "User denied the request for Geolocation.";
-            break;
-        case error.POSITION_UNAVAILABLE:
-            x.innerHTML = "Location information is unavailable.";
-            break;
-        case error.TIMEOUT:
-            x.innerHTML = "The request to get user location timed out.";
-            break;
-    }
-}
-
-//socket
-
-function setConnected(connected) {
-    document.getElementById('connect').disabled = connected;
-    document.getElementById('disconnect').disabled = !connected;
-}
-var socket = new SockJS('http://test.pusher.com/'); //http://chataround.ddns.net:8080/login
-var stompClient = Stomp.over(socket);
-
-function connect() {
-    stompClient.connect({}, function(frame) {
-        setConnected(true);
-        console.log('Connected: ' + frame);
-        stompClient.subscribe('/topic/chat', function(LoginMsg){
-            showLoginMesssage(JSON.parse(LoginMsg.body).content);
-        });
-    });
-}
-
-function showLoginMesssage(message) {
-    var response = document.getElementById('response');
-    var p = document.createElement('p');
-    p.style.wordWrap = 'break-word';
-    p.appendChild(document.createTextNode(message));
-    response.appendChild(p);
-}
-function disconnect() {
-    if (stompClient != null) {
-        stompClient.disconnect();
-    }
-    setConnected(false);
-    console.log("Disconnected");
-}
-
-function sendLogin(){
-
-    var username = $('#username').val();
-    //var password = $('#password').val();
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    var id = "1L";
-
-    var userInfo = {
+    var data = {
         id: id,
         username: username,
-        //password: password,
         latitude: latitude,
         longitude: longitude
     };
 
-    socket.send(JSON.stringify(userInfo));
-}
+    $.post("http://chataround.ddns.net:8080/login", data)
+        .done(function (response) {
+            window.location.href = "html/main.html";
+        })
+        .fail(function (error) {
+            window.location.href = "html/main.html";
+            console.log(error);
+        });
+};
+
+$("[name='login']").on("click", sendLoginInfo);
+
+// Sockets
+
+//function setConnected(connected) {
+//    document.getElementById('connect').disabled = connected;
+//    document.getElementById('disconnect').disabled = !connected;
+//}
+//
+//var socket = new SockJS('http://test.pusher.com/'); //http://chataround.ddns.net:8080/login
+//var stompClient = Stomp.over(socket);
+//
+//function connect() {
+//    stompClient.connect({}, function (frame) {
+//        setConnected(true);
+//        console.log('Connected: ' + frame);
+//        stompClient.subscribe('/topic/chat', function (LoginMsg) {
+//            showLoginMesssage(JSON.parse(LoginMsg.body).content);
+//        });
+//    });
+//}
+//
+//function showLoginMesssage(message) {
+//    var response = document.getElementById('response');
+//    var p = document.createElement('p');
+//    p.style.wordWrap = 'break-word';
+//    p.appendChild(document.createTextNode(message));
+//    response.appendChild(p);
+//}
+//function disconnect() {
+//    if (stompClient != null) {
+//        stompClient.disconnect();
+//    }
+//    setConnected(false);
+//    console.log("Disconnected");
+//}
+
+//function sendLogin(){
+//
+//    var username = $('#username').val();
+//    //var password = $('#password').val();
+//    var latitude = position.coords.latitude;
+//    var longitude = position.coords.longitude;
+//    var id = "1L";
+//
+//    var userInfo = {
+//        id: id,
+//        username: username,
+//        //password: password,
+//        latitude: latitude,
+//        longitude: longitude
+//    };
+//
+//    socket.send(JSON.stringify(userInfo));
+//}
