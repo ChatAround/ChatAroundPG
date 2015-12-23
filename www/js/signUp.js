277f77d66d6bb85c4ca5ba9df10d1ce896ce8029
Stratos Balis <strbalis@gmail.com>
2015-12-23 17:28:32 +0200

    Create sign up form Changes in login.css, index.html

---
+++ www/js/signUp.js
@@ -0,0 +1,149 @@
+/**
+ * Created by Stratos on 12/20/2015.
+ */
+
+var NS = {
+    currentLatitude: null,
+    currentLongitude: null
+};
+var userName = $.cookie('userName');
+var password = $.cookie('password');
+var uMessage;
+var pMessage;
+
+var Location = {
+    updateLocation: function () {
+        var updateCurrentLocation = function (position) {
+            NS.currentLatitude = position.coords.latitude;
+            NS.currentLongitude = position.coords.longitude;
+            showUserName();
+            showUserId();
+            showLocation();
+        };
+
+        var throwError = function (error) {
+            switch (error.code) {
+                case error.PERMISSION_DENIED:
+                    throw new Error("User denied the request for Geolocation.");
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    throw new Error("Location information is unavailable.");
+                    break;
+                case error.TIMEOUT:
+                    throw new Error("The request to get user location timed out.");
+                    break;
+            }
+        };
+
+        navigator.geolocation.getCurrentPosition(updateCurrentLocation, throwError);
+    }
+};
+
+var sendRegisterInfo = function(e) {
+    e.preventDefault();
+
+    var firstname = $("[name='first_name']").val();
+    var lastname = $("[name='last_name']").val();
+    var username = $("[name='username']").val();
+    var password = $("[name='password']").val();
+    var confirmPass = $("[name='confirm_pass']").val();
+    var gender = $("[name='gender']").val();
+    var country = $("[name='country']").val();
+    var city = $("[name='city']").val();
+    var birthday = $("[name='birthday']").val();
+    var about = $("[name='about']").val();
+    var latitude = NS.currentLatitude;
+    var longitude = NS.currentLongitude;
+    var isOnline = true;
+    $.cookie('userName', username);
+    $.cookie('password', password);
+
+    var userData = {
+        username: username,
+        password: password,
+        latitude: latitude,
+        longitude: longitude,
+        isOnline: isOnline
+    };
+
+    var profileData = {
+        username: username,
+        firstname: firstname,
+        lastname: lastname,
+        gender: gender,
+        country: country,
+        city: city,
+        birthday: birthday,
+        about: about
+    };
+
+    if(firstname=="") {
+        document.getElementById("lfirst_name").style.display = "inline";
+    } else if(lastname=="") {
+        document.getElementById("llast_name").style.display = "inline";
+    } else if(username=="") {
+        document.getElementById("lusername").style.display = "inline";
+    } else if(password=="" || confirmPass=="") {
+        document.getElementById("lpassword").style.display = "inline";
+        document.suform.password.value = "";
+        document.suform.confirm_pass.value = "";
+    } else if(password!=confirmPass) {
+        document.getElementById("lconfirm_pass").style.display = "inline";
+        document.suform.confirm_pass.value = "";
+    }  else {
+        $.post("http://chataround.ddns.net:8080/user", userData)
+            .done(function (uResponse) {
+                uMessage = uResponse;
+                if (uMessage == "OK") {
+                    $.post("http://chataround.ddns.net:8080/userProfile", profileData)
+                        .done(function (pResponse) {
+                            pMessage = pResponse;
+                            if (pMessage == "OK") {
+                                window.location.href = "../index.html";
+                            } else {
+                                window.alert(pMessage)
+                            }
+                        })
+                        .fail(function (pError) {
+                            console.log(pError);
+                        });
+                } else {
+                    window.alert(uMessage)
+                }
+            })
+            .fail(function (uError) {
+                console.log(uError);
+            });
+    }
+
+
+};
+
+function hideFNLabel() {
+    document.getElementById("lfirst_name").style.display = "none";
+}
+function hideLNLabel() {
+    document.getElementById("llast_name").style.display = "none";
+}
+function hideUNLabel() {
+    document.getElementById("lusername").style.display = "none";
+}
+function hidePLabel() {
+    document.getElementById("lpassword").style.display = "none";
+}
+function hidePCLabel() {
+    document.getElementById("lconfirm_pass").style.display = "none";
+}
+function hideELabel() {
+    document.getElementById("lemail").style.display = "none";
+}
+
+
+$("[name='first_name']").on("click", hideFNLabel);
+$("[name='last_name']").on("click", hideLNLabel);
+$("[name='username']").on("click", hideUNLabel);
+$("[name='password']").on("click", hidePLabel);
+$("[name='confirm_pass']").on("click", hidePCLabel);
+$("[name='email']").on("click", hideELabel);
+
+$("[name='register']").on("click", sendRegisterInfo);
