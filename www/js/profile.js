5bece3f971d9d62927d690382db3c08cb139abf0
Stratos <strbalis@gmail.com>
2016-01-12 17:28:39 +0200

    Updated page.css changed logo in: main.html, otherProfile.html, profile.html Added img/img folder

--- www/js/profile.js
+++ www/js/profile.js
@@ -1,67 +1,67 @@
 /**
     * @author Stratos
 */
-//var userName = $.cookie('userName');
-//
-//    //get and update profile values
-//
-//    var userProfileModel = Backbone.Model.extend({
-//        defaults: {
-//            "username": "",
-//            "firstName": "",
-//            "surName": "",
-//            "gender": "",
-//            "country": "",
-//            "city": "",
-//            "birthday": "",
-//            "about": ""
-//        }
-//    });
-//
-//    var userProfileCollection = Backbone.Collection.extend({
-//        url: 'http://chataround.ddns.net:8080/userProfile' + '?' + $.param({username: userName}),
-//        model: userProfileModel
-//    });
-//
-//    var UserProfile = new userProfileCollection();
-//
-//    var getProfile = function(UserProfile) {
-//        $('#prof_user').val(UserProfile.username);
-//        $('#firstname').val(UserProfile.firstName);
-//        $('#lastname').val(UserProfile.surName);
-//        $('#gender').val(UserProfile.gender);
-//        $('#country').val(UserProfile.country);
-//        $('#city').val(UserProfile.city);
-//        $('#birthday').val(UserProfile.birthday);
-//        $('#about').val(UserProfile.about);
-//    };
-//
-//    UserProfile.fetch()
-//        .then(getProfile);
-//
-//    var updateProfile = function() {
-//        var username = document.getElementById("prof_user").value;
-//        var firstname = document.getElementById("firstname").value;
-//        var lastname = document.getElementById("lastname").value;
-//        var country = document.getElementById("country").value;
-//        var city = document.getElementById("city").value;
-//        var birthday = document.getElementById("birthday").value;
-//        var gender = document.getElementById("gender").value;
-//        var about = document.getElementById("about").value;
-//        var birth = birthday.replace(/-/g, "/");
-//
-//        $.ajax({
-//                type: 'PUT',
-//                url: 'http://chataround.ddns.net:8080/userProfile' + '?' + $.param({username: userName,firstName: firstname, surName: lastname, gender: gender, country: country, city: city, birthday: birth, about:about})
-//            })
-//            .done(function(result) {
-//                window.alert("Changes saved!")
-//            })
-//    };
+var userName = $.cookie('userName');
+
+    //get and update profile values
+
+    var userProfileModel = Backbone.Model.extend({
+        defaults: {
+            "username": "",
+            "firstName": "",
+            "surName": "",
+            "gender": "",
+            "country": "",
+            "city": "",
+            "birthday": "",
+            "about": ""
+        }
+    });
+
+    var userProfileCollection = Backbone.Collection.extend({
+        url: 'http://chataround.ddns.net:8080/userProfile' + '?' + $.param({username: userName}),
+        model: userProfileModel
+    });
+
+    var UserProfile = new userProfileCollection();
+
+    var getProfile = function(UserProfile) {
+        $('#prof_user').val(UserProfile.username);
+        $('#firstname').val(UserProfile.firstName);
+        $('#lastname').val(UserProfile.surName);
+        $('#gender').val(UserProfile.gender);
+        $('#country').val(UserProfile.country);
+        $('#city').val(UserProfile.city);
+        $('#birthday').val(UserProfile.birthday);
+        $('#about').val(UserProfile.about);
+    };
+
+    UserProfile.fetch()
+        .then(getProfile);
+
+    var updateProfile = function() {
+        var username = document.getElementById("prof_user").value;
+        var firstname = document.getElementById("firstname").value;
+        var lastname = document.getElementById("lastname").value;
+        var country = document.getElementById("country").value;
+        var city = document.getElementById("city").value;
+        var birthday = document.getElementById("birthday").value;
+        var gender = document.getElementById("gender").value;
+        var about = document.getElementById("about").value;
+        var birth = birthday.replace(/-/g, "/");
+
+        $.ajax({
+                type: 'PUT',
+                url: 'http://chataround.ddns.net:8080/userProfile' + '?' + $.param({username: userName,firstName: firstname, surName: lastname, gender: gender, country: country, city: city, birthday: birth, about:about})
+            })
+            .done(function(result) {
+                window.alert("Changes saved!")
+            })
+    };
 
 
 //edit profile
-//window.onload = function() {
+window.onload = function() {
     document.getElementById("editCo").onclick = function() {
         document.getElementById("country").disabled = false;
         document.getElementById("country").style.background = 'white';
@@ -145,6 +145,6 @@
             document.getElementById("about").style.background = 'rgba(50, 23, 77, 0)';
         }
     };
-//};
+};
 
 $("[name='saveProfile']").on('click', updateProfile);
