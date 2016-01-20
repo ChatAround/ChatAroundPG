7228e4ee1181399df9826fb47af194b06e4cc1c7
Stratos <strbalis@gmail.com>
2016-01-20 18:05:20 +0200

    Updated main.html, popup.js, main.css, page.css

--- www/js/popup.js
+++ www/js/popup.js
@@ -33,45 +33,59 @@ document.getElementById("settingsBtn").onclick = function(){
 };
 
 document.getElementById("closeBtn").onclick = function() {
-    document.getElementById("overlay").style.display = "none";
-    document.getElementById("popup").style.display = "none";
+    if (document.getElementById("radius").value.length < 1) {
+        document.getElementById("timeError").style.display = 'none';
+        document.getElementById("radiusError").style.display = 'block';
+        document.getElementById("radius").focus();
+    } else if (document.getElementById("selectedTime").value.length < 1) {
+        document.getElementById("radiusError").style.display = 'none';
+        document.getElementById("timeError").style.display = 'block';
+        document.getElementById("selectedTime").focus();
+    } else {
+        document.getElementById("radiusError").style.display = 'none';
+        document.getElementById("timeError").style.display = 'none';
+        document.getElementById("overlay").style.display = "none";
+        document.getElementById("popup").style.display = "none";
+    }
 };
 
 document.getElementById("radius").onchange = function() {
-    var newRadius = document.getElementById("radius").value;
-    var SSRadius = $.cookie('sRadius');
-    if (newRadius != 1000 && newRadius != SSRadius ) {
-        $.cookie('sRadius', newRadius);
-        var SRadius = $.cookie('sRadius');
-        document.getElementById("radius").defaultValue = SRadius;
-    } else {
-        document.getElementById("radius").value = document.getElementById("radius").defaultValue;
-    }
+        var newRadius = document.getElementById("radius").value;
+        var SSRadius = $.cookie('sRadius');
+        if (newRadius != 1000 && newRadius != SSRadius ) {
+            $.cookie('sRadius', newRadius);
+            var SRadius = $.cookie('sRadius');
+            document.getElementById("radius").defaultValue = SRadius;
+            console.log(document.getElementById("radius").value.length);
+            console.log(document.getElementById("selectedTime").value);
+        } else {
+            document.getElementById("radius").value = document.getElementById("radius").defaultValue;
+        }
 
-};
+    };
 
-document.getElementById("selectedTime").onchange = function() {
-    var newTime = document.getElementById("selectedTime").value;
-    var SSTime = $.cookie('sTime');
-    if (newTime != 1000 && newTime != SSTime ) {
-        $.cookie('sTime', newTime);
-        var STime = $.cookie('sTime');
-        document.getElementById("selectedTime").defaultValue = STime;
-    } else {
-        document.getElementById("selectedTime").value = document.getElementById("selectedTime").defaultValue;
-    }
-};
+    document.getElementById("selectedTime").onchange = function() {
+        var newTime = document.getElementById("selectedTime").value;
+        var SSTime = $.cookie('sTime');
+        if (newTime != 1000 && newTime != SSTime ) {
+            $.cookie('sTime', newTime);
+            var STime = $.cookie('sTime');
+            document.getElementById("selectedTime").defaultValue = STime;
+        } else {
+            document.getElementById("selectedTime").value = document.getElementById("selectedTime").defaultValue;
+        }
+    };
 
-document.getElementById("duration").onchange = function() {
-    var newDuration = document.getElementById("duration").value;
-    var SSDuration = $.cookie('sDuration');
-    if (newDuration != 1000 && newDuration != SSDuration ) {
-        $.cookie('sDuration', newDuration);
-        var SDuration = $.cookie('sDuration');
-        document.getElementById("duration").defaultValue = SDuration;
-    } else {
-        document.getElementById("duration").value = document.getElementById("duration").defaultValue;
-    }
+    document.getElementById("duration").onchange = function() {
+        var newDuration = document.getElementById("duration").value;
+        var SSDuration = $.cookie('sDuration');
+        if (newDuration != 1000 && newDuration != SSDuration) {
+            $.cookie('sDuration', newDuration);
+            var SDuration = $.cookie('sDuration');
+            document.getElementById("duration").defaultValue = SDuration;
+        } else {
+            document.getElementById("duration").value = document.getElementById("duration").defaultValue;
+        }
 };
 
 function isNumber(evt) {
